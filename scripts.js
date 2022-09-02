2516167b6e9ba0d3a62f513d7836714158170b07
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-01 22:31:31 -0500

    Database in an object.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,43 @@
-
+let DB = {
+    m4: 
+    {
+    name: "M416", 
+    image: "https://i.postimg.cc/02YQdfRT/M4.png",
+    },  
+    AK: 
+    {
+    name: "AKM", 
+    image: "https://i.postimg.cc/zXZvgQMw/AK.png",
+    },  
+    Beryl: 
+    {
+    name: "Beryl", 
+    image: "https://i.postimg.cc/7PTbb4BY/Beryl.png",
+    },  
+    AWM: 
+    {
+    name: "AWM", 
+    image: "https://i.postimg.cc/SR8KPLXz/AWM.png",
+    },  
+    M24: 
+    {
+    name: "M24", 
+    image: "https://i.postimg.cc/vBCBX2ms/M24.png",
+    },  
+    KAR: 
+    {
+    name: "Kar98K", 
+    image: "https://i.postimg.cc/QtFCQZhm/KAR.png",
+    },  
+    SLR: 
+    {
+    name: "SLR", 
+    image: "https://i.postimg.cc/0N2bNJ4L/SLR.png",
+    },  
+    SKS: 
+    {
+    name: "SKS", 
+    image: "https://i.postimg.cc/xdxqN1Dq/SKS.png",
+    },  
+};
 
